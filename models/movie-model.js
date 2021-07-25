7501aa6692a2a24c14f6bc28f4987ee9179cc2c6
Kostiantyn Teltov <dneprokos@gmail.com>
2021-07-25 11:58:56 +0300

    Application start logic was moved to separate module

--- models/movie-model.js
+++ models/movie-model.js
@@ -1,21 +1,5 @@
-const Joi = require('joi');
-
-function validateProperties(id, name, release_date, genre_ids) {
-    const schema = {
-        id: Joi.number().required().min(1),
-        name: Joi.string().required().min(3),
-        release_date: Joi.number().min(1930).optional(),
-        genre_ids: Joi.array().items(Joi.number()).optional()
-    };
-
-    return Joi.validate({id, name, release_date, genre_ids}, schema);
-}
-
 module.exports = class Movie {
     constructor(id, name, release_date = null, genre_ids = null) {
-        const { error } = validateProperties(id, name, release_date, genre_ids);
-        if (error) throw new Error("Movie class validation failed with an error:" + error.details[0].message);
-
         this.id = id;
         this.name = name;
         this.release_date = release_date,
