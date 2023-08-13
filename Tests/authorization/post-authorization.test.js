547efec1fb1eaa8951061851b9e765524f56eb3e
Kostiantyn Teltov <dneprokos@gmail.com>
2023-08-13 08:24:14 +0200

    Auth API tests were finished

--- Tests/authorization/post-authorization.test.js
+++ Tests/authorization/post-authorization.test.js
@@ -3,6 +3,7 @@ const { AuthorizationApi } = require('@api');
 const envConfig = require('@env');
 
 describe('POST - Authorization API Tests', () => {
+    
     it('Send non-admin user auth-Non admin user token should be returned', async () => {
         //Arrange
 
@@ -13,5 +14,29 @@ describe('POST - Authorization API Tests', () => {
         //Assert
         expect(response.status).toBe(200);
         expect(response.data.accessToken).toBeDefined();
+    });
+
+    it('Send admin user auth-Admin user token should be returned', async () => {
+        //Arrange
+
+        //Act
+        const response = await new AuthorizationApi()
+            .getAuthToken(envConfig.adminUser, envConfig.adminPassword);
+        
+        //Assert
+        expect(response.status).toBe(200);
+        expect(response.data.accessToken).toBeDefined();
+    });
+
+    it('Send invalid user auth-404 should be returned', async () => {
+        //Arrange
+
+        //Act
+        const response = await new AuthorizationApi()
+            .getAuthToken("JonDou", "Password123");
+        
+        //Assert
+        expect(response.status).toBe(404);
+        expect(response.data).toBe('Invalid credentials.');
     });
 });
